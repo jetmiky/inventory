da29a725b42e3f9438a7655a76393ef89e8460a9
Bima Jatmiko Abadi <bja.devs@gmail.com>
2024-12-22 06:47:04 +0700

    Add cru feature of inventory order payments

--- app/(defaults)/inventory-orders/[id]/page.tsx
+++ app/(defaults)/inventory-orders/[id]/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { Metadata } from 'next';
 import ComponentsTablesInventoryOrderDetails from '@/components/tables/components-tables-inventory-order-details';
 import ComponentsTablesInventoryOrderPayments from '@/components/tables/components-tables-inventory-order-payments';
+import prisma from '@/prisma/client';
 
 export const metadata: Metadata = {
     title: 'Inventory Order',
@@ -13,6 +14,16 @@ type InventoryOrderPageProps = {
 
 const InventoryOrder = async ({ params }: InventoryOrderPageProps) => {
     const id = (await params).id;
+
+    const order = await prisma.inventoryOrder.findFirst({
+        where: { id: Number.parseInt(id) },
+        include: { supplier: true, payments: { include: { method: true } } },
+    });
+
+    const methods = await prisma.supplierPaymentMethod.findMany({
+        orderBy: { name: 'asc' },
+        where: { supplierId: order?.supplierId },
+    });
 
     return (
         <div className="grid grid-cols-1 gap-6">
@@ -27,7 +38,7 @@ const InventoryOrder = async ({ params }: InventoryOrderPageProps) => {
             <div className="mb-5 flex items-center justify-center">
                 <div className="w-full rounded-lg border border-white-light bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
                     <div className="px-6 py-7">
-                        <ComponentsTablesInventoryOrderPayments />
+                        <ComponentsTablesInventoryOrderPayments order={order} methods={methods} />
                     </div>
                 </div>
             </div>
