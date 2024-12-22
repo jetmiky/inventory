9e1547f3a4af63db7e537a99f219b551f8049fe3
Bima Jatmiko Abadi <bja.devs@gmail.com>
2024-12-22 18:55:29 +0700

    Style inventory management page

--- app/(defaults)/inventories/page.tsx
+++ app/(defaults)/inventories/page.tsx
@@ -24,7 +24,7 @@ const Inventories = async () => {
     return (
         <div className="grid grid-cols-1 gap-6">
             <div className="mb-5 flex items-center justify-center">
-                <div className="w-full rounded-lg border border-white-light bg-white shadow-[4px_6px_10px_-3px_#bfc9d4] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
+                <div className="w-full rounded-lg border border-white-light bg-white shadow-[4px_6px_10px_-3px_#edf0f2] dark:border-[#1b2e4b] dark:bg-[#191e3a] dark:shadow-none">
                     <div className="px-6 py-7">
                         <ComponentsTablesInventories inventories={inventories} types={types} brands={brands} />
                     </div>
